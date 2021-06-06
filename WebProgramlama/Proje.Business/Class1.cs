bbde36e249a8607eeb30d3818ec9c7c72b64575c
Kübra Uçkun <49203387+kubrauckun@users.noreply.github.com>
2021-06-06 14:10:26 +0300

    Entity kurulumu / veritabanı bağlantısı

--- WebProgramlama/Proje.Business/Class1.cs
+++ WebProgramlama/Proje.Business/Class1.cs
@@ -8,5 +8,32 @@ namespace Proje.Business
 {
     public class Class1
     {
+        public List<Proje.DataAccess.siparisVerileri> Listele()  
+        {
+            Proje.DataAccess.cicekciTablosuEntities nsn = new Proje.DataAccess.cicekciTablosuEntities(); 
+            var sonuc = nsn.siparisVerileri.OrderBy(p => p.id).ToList(); 
+            return sonuc; 
+
+        }
+
+        public string veriGirisi(string adSoyad, string mail, string phone, string message, string cicekTuru) 
+        {
+            Proje.DataAccess.cicekciTablosuEntities entity = new DataAccess.cicekciTablosuEntities();
+
+            Proje.DataAccess.siparisVerileri ekleme = new DataAccess.siparisVerileri(); 
+
+            ekleme.adSoyad = adSoyad;
+            ekleme.mail = mail;
+            ekleme.phone = phone;
+            ekleme.message = message;
+            ekleme.cicekTuru = cicekTuru;
+
+            entity.siparisVerileri.Add(ekleme); 
+            entity.SaveChanges();
+
+            var veriListele = Listele();
+
+            return "1";
+        }
     }
 }
